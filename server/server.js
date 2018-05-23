d761a18baa2b89f095e035fb602edcbc284f11ad
Manav <manavbajaj1997@gmail.com>
2018-05-24 00:58:48 +0530

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -33,6 +35,28 @@ app.get('/todos',(req,res)=>{
         res.send({todos})
     },(err)=>{
         res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        //success case  
+       
+       if(!todo){
+      return  res.status(404).send();
+       } 
+
+       res.send({todo});
+        
+    },(e)=>{
+        res.status(400).send(e);
     });
 });
 
