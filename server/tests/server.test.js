a9b742ab85d704a53803fc0baff415e9facc0835
Manav <manavbajaj1997@gmail.com>
2018-05-26 19:54:40 +0530

    ADD test for POST/users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 
 const {app} = require('./../server.js');
 const {Todo}= require('./../models/todo');
+const {User}=  require('./../models/user');
 
 const {ObjectID} = require('mongodb');
 
@@ -208,4 +209,76 @@ describe('PATCH /todos/:id', () => {
 
         .end(done);
     });
+
+    it('should return 401 if not authenticated',(done)=>{
+
+        request(app)
+        .get('/users/me')
+        .expect(401)
+        .expect((res)=>{
+            expect(res.body).toEqual({})
+        })
+
+        .end(done);
+    })
+  });
+
+  describe('POST/users',()=>{
+      it('should created a user',(done)=>{
+          var email = 'example@mail.com'
+          var password = '123vbc'
+          request(app)
+          .post('/users')
+          .send({email,password})
+          .expect(200)
+          .expect((res)=>{
+              expect(res.headers['x-auth']).toExist();
+              expect(res.body.email).toBe(email);
+              expect(res.body._id).toExist();
+            
+            
+          })
+          .end((err)=>{
+
+            if(err){
+                return done(err);
+            }
+
+            User.findOne({email}).then((user)=>{
+                expect(user).toExist()
+                expect(user.password).toNotBe(password);
+                done();
+            })
+          });
+      });
+
+      it('should return validation errors if request invalid',(done)=>{
+            
+              var email = 'man';
+              var password = '44';
+
+              request(app)
+              .post('/users')
+              .send({
+                  email,
+                  password
+              })
+              .expect(400)
+              .end(done);
+      })
+
+      it('should not create user if email in use',(done)=>{
+
+        var email ='ajay@example.com';
+        var password='123abc'
+        request(app)
+        .post('/users')
+        .send({
+            email,
+            password
+        })
+        .expect(400)
+        .end(done);
+
+      })
   });
